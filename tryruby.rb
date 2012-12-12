fbd33fb3cf704c21bcb3ceff18c0cc05a1a15893
Terence Lee <hone02@gmail.com>
2012-12-12 11:39:06 -0800

    try to use pipes

--- tryruby.rb
+++ tryruby.rb
@@ -16,10 +16,32 @@ data = heroku.post_ps(
   { :attach => true, :ps_env => env }
 ).body
 
-rendezvous = Rendezvous.new(url: data['rendezvous_url'],
-                            input: $stdin,
-                            output: $stdout)
+parent_read, child_write = IO.pipe
+child_read, parent_write = IO.pipe
 
-Rendezvous.start(
-  :url => data['rendezvous_url']
-)
+rendezvous = Rendezvous.new(url:    data['rendezvous_url'],
+                            input:  child_read,
+                            output: child_write)
+
+child_pid = fork do
+  parent_read.close
+  parent_write.close
+  rendezvous.start
+end
+
+read_pid = fork do
+  parent_write.close
+  until parent_read.eof?
+    print parent_read.readpartial(4096)
+  end
+end
+
+parent_write.write("ls\n")
+parent_write.write("exit\n")
+
+Process.wait(read_pid)
+
+parent_read.close
+parent_write.close
+
+Process.wait(child_pid)
